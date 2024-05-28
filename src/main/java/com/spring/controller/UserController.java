127a92df734c5512c12141ff048cf93a0217d0a9
sourabh sondawale <sourabh.sondawale0@gmail.com>
2024-05-28 18:53:29 +0530

    Premium model class added

---
+++ src/main/java/com/spring/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.spring.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	public ResponseEntity<User>
+}
